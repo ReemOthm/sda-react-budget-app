ed859cf36923c8be7a751e075371aa73305d2cad
ReemOthm <ReemOth@outlook.com>
2024-03-06 19:55:15 +0300

    implemented add target feature

--- src/Components/Target.tsx
+++ src/Components/Target.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from "react";
 import Button from "./UI/Button";
 import Input from "./UI/Input";
 
@@ -6,15 +7,25 @@ interface IProps {
 }
 
 const Target = ({}:IProps)=>{
+
+    // ---------------STATES-----------------
+    const [target,setTarget] = useState(0)
+
+    // ---------------HANDLERS-----------------
+    const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
+        const {value} = e.target;
+        setTarget(Number(value))
+    }
+
     return (
         <form>
             <label htmlFor="target">
                 Set Target
-                <Input  type="numer" name="target" id="target" required/>
+                <Input type="numer" name="target" id="target" onChange={handleChange} required/>
             </label>
             <input type="reset" value='Reset' />
             <p>Current Saving: 0</p>
-            <p>Target: 0</p>
+            <p>Target: {target}</p>
             <label htmlFor="progress">
                 Progress: 0% 
                 <progress id="progress" value={0} max={100} />
