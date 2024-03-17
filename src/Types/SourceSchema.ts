012e481c42c27945c75173b172a2490a5d218bd3
Reem <86829326+ReemOthm@users.noreply.github.com>
2024-03-17 03:49:23 +0300

    Update SourceSchema.ts

--- src/Types/SourceSchema.ts
+++ src/Types/SourceSchema.ts
@@ -2,8 +2,7 @@ import { z } from "zod";
 
 export const SourceSchema = z.object({
     id:  z.string().optional(),
-    source: z.string().min(4,{message: 'source must be more than 4 characters'})
-        .max(20, {message: "source should be less than 20 character!"})
+    source: z.string().min(1,{message: 'source must be more than 4 characters'})
         .regex(/^\s*\S/, {message: "White-spaces aren't accepted!"}),
     amount: z.coerce.number().positive({message: "amount must be a positive number and more than 0"}),
     date: z.string().min(1, {message: 'This field is required'})
